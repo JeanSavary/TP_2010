859dae2385dc0a8c02694ae1ffff23bddeee867f
badrlab <37595767+badrlab@users.noreply.github.com>
2018-04-09 16:28:26 -0400

    Done

--- TP5/Node.java
+++ TP5/Node.java
@@ -57,27 +57,77 @@ public class Node {
 
 	public Node fusion(Node autre) throws DifferentOrderTrees {
 		// A completer
-		return null;
+        if (autre != null && ordre == autre.ordre ){
+        	if (valeur > autre.valeur){
+        		autre.parent = this;
+        		ordre++;
+        		enfants.add(autre);
+        		return this;
+        	}
+        	else{	
+            	parent = autre;
+            	autre.ordre++;
+            	autre.enfants.add(this);
+        		return parent;
+        	}
+        }
+        else  throw new DifferentOrderTrees();
 	}
 
 	private void moveUp() {
 		// A completer
+		ArrayList<Node> enfants = this.enfants;
+    	Node parent = this.parent;
+    	int ordre= this.ordre;
+    	
+    	this.parent= parent.parent;
+    	parent.parent=this;
+    	
+    	this.ordre=parent.ordre;
+    	parent.ordre=ordre;
+    	
+    	this.enfants=parent.enfants;
+    	this.enfants.add(parent);
+    	this.enfants.remove(this);
+    	parent.enfants=enfants;
+    	if(this.parent != null) {
+    		this.parent.enfants.add(this);
+    		this.parent.enfants.remove(parent);
+    	}
 	}
 
 	public ArrayList<Node> delete() {
-		// A completer
-		return null;
+		while(this.parent != null) this.moveUp();
+		for(int i = 0; i < this.enfants.size(); i++) this.enfants.get(i).parent = null;
+		return this.enfants;
 	}
 
 	public Node findValue(int valeur) {
 		// A completer
+		if (this.valeur == valeur)
+			return this;
+		else if ( this.valeur > valeur) {
+			for ( int i = 0; i < this.enfants.size(); i++ ) {
+				Node temp = this.enfants.get(i).findValue(valeur);
+				if ( temp != null ) return temp;
+			}
+		}
 		return null;
 	}
 
 	public ArrayList<Integer> getElementsSorted() {
-		// A completer
-		return null;
-	}
+    	ArrayList<Integer> valeurs = new ArrayList<Integer>();
+    	valeurs.add(valeur);
+    	ArrayList<Node> nodeList = getEnfants();
+    	while(nodeList.size() > 0){
+            Node smallestNode = nodeList.get(0);
+            for(Node n : nodeList) if(n.getVal() > smallestNode.getVal()) smallestNode = n;
+            valeurs.add(smallestNode.getVal());
+            nodeList.remove(smallestNode);
+            nodeList.addAll(smallestNode.getEnfants());
+    	}
+    	return valeurs;
+    }
 
 	public void print() {
         print("", true);
